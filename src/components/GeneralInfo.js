306646ca0e848b7a6747a14ba7a4f76dbd3e4cb4
JoaoMateus98 <>
2022-12-20 15:02:26 -0500

    handleChange function works to update state properties

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -12,7 +12,32 @@ class GeneralInfo extends React.Component {
     };
   }
 
+  handleChange = (e) => {
+    // TODO: update the state properties
+
+    switch (e.target.id) {
+      case "name":
+        this.setState({
+          name: e.target.value,
+        });
+        break;
+      case "email":
+        this.setState({
+          email: e.target.value,
+        });
+        break;
+      case "phone":
+        this.setState({
+          phone: e.target.value,
+        });
+        break;
+      default:
+        console.log("invalid target");
+    }
+  };
+
   handleSubmit = (e, name) => {
+    // TODO: should pass all state properties back to app
     this.props.handleGeneralInfoUpdate(e, name);
   };
 
@@ -21,7 +46,13 @@ class GeneralInfo extends React.Component {
       <form className="general-main-container" onSubmit={this.handleSubmit}>
         <div className="divider">
           <label htmlFor="name">Name: </label>
-          <input type={"text"} id="name" placeholder="Enter Name Here"></input>
+          <input
+            type={"text"}
+            id="name"
+            placeholder="Enter Name Here"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
         </div>
         <div className="divider">
           <label htmlFor="email">Email: </label>
@@ -29,6 +60,8 @@ class GeneralInfo extends React.Component {
             type={"email"}
             id="email"
             placeholder="Enter Email Here"
+            onChange={this.handleChange}
+            value={this.state.email}
           ></input>
         </div>
         <div className="divider">
@@ -37,6 +70,8 @@ class GeneralInfo extends React.Component {
             type={"number"}
             id="phone"
             placeholder="Enter Name Here"
+            onChange={this.handleChange}
+            value={this.state.phone}
           ></input>
         </div>
         <div className="divider">
