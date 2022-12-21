b5454dbf9200fe2a08a217ce147e1eb9c3f1af41
JoaoMateus98 <joaobsu98@gmail.com>
2022-12-21 17:45:12 -0500

    reset project

--- src/components/Education.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Education extends React.Component {
-  render() {
-    return <div>This is Education</div>;
-  }
-}
-
-export default Education;
--- src/components/Professional.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Professional extends React.Component {
-  render() {
-    return <div>This is Professional</div>;
-  }
-}
-
-export default Professional;
