5dd49a7d8127b6878305b571a09586bd357f5b64
JoaoMateus98 <joaobsu98@gmail.com>
2022-12-22 08:49:37 -0500

    GI_Display nows takes a function prop to turn on edit mode

--- src/components/GI_Components/GI_Display.js
+++ src/components/GI_Components/GI_Display.js
@@ -1,14 +1,16 @@
 import React from "react";
 import DisplayItem from "../HelperComponents/DisplayItem";
+import EditButton from "../HelperComponents/EditButton";
 
 class GI_Display extends React.Component {
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone, handleEditMode } = this.props;
     return (
       <div>
         <DisplayItem category={"Name"} value={name} />
         <DisplayItem category={"Email"} value={email} />
         <DisplayItem category={"Phone"} value={phone} />
+        <EditButton handleEditMode={handleEditMode} />
       </div>
     );
   }
