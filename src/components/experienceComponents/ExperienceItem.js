95ea48497575e7d08d83c6060b4c7d3a82a74134
JoaoMateus98 <joaobsu98@gmail.com>
2022-12-25 19:40:44 -0500

    proper info is displaying when adding job. ExperienceItem needs state

--- src/components/experienceComponents/ExperienceItem.js
+++ src/components/experienceComponents/ExperienceItem.js
@@ -1,8 +1,38 @@
 import React from "react";
 
 class ExperienceItem extends React.Component {
+  constructor(props) {}
+
   render() {
-    return <div>job place holder</div>;
+    const jobObj = this.props.jobObj;
+
+    console.log(jobObj);
+    return (
+      <div className="job-container">
+        <div className="job-company">
+          <p>Company:</p>
+          <p>{jobObj.company}</p>
+        </div>
+        <div className="job-position">
+          <p>Position:</p>
+          <p>{jobObj.position}</p>
+        </div>
+        <div className="job-description">
+          <p>Description:</p>
+          <p>{jobObj.description}</p>
+        </div>
+        <div className="job-period">
+          <div>
+            <p>Start Date:</p>
+            <p>{jobObj.period.start}</p>
+          </div>
+          <div>
+            <p>End Date:</p>
+            <p>{jobObj.period.end}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
