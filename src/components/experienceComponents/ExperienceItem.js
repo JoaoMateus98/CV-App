4641f550a7520b736ff6af20e4bf6e6b391fcf45
JoaoMateus98 <joaobsu98@gmail.com>
2022-12-26 16:39:43 -0500

    starting delete button now

--- src/components/experienceComponents/ExperienceItem.js
+++ src/components/experienceComponents/ExperienceItem.js
@@ -21,35 +21,34 @@ class ExperienceItem extends React.Component {
     const jobObj = this.props.jobObj;
     return (
       <div className="job-container">
-        <div className="job-company">
+        <div className="job-field">
           <p>Company:</p>
           <p>{jobObj.company}</p>
         </div>
-        <div className="job-position">
+        <div className="job-field">
           <p>Position:</p>
           <p>{jobObj.position}</p>
         </div>
-        <div className="job-description">
+        <div className="job-field">
           <p>Description:</p>
           <p>{jobObj.description}</p>
         </div>
-        <div className="job-period">
-          <div>
-            <p>Start Date:</p>
-            <p>{jobObj.period.start}</p>
-          </div>
-          <div>
-            <p>End Date:</p>
-            <p>{jobObj.period.end}</p>
-          </div>
-          <button
-            onClick={() => {
-              this.handleEdit(context, jobObj);
-            }}
-          >
-            Edit
-          </button>
+        <div className="job-field">
+          <p>Start Date:</p>
+          <p>{jobObj.period.start}</p>
         </div>
+        <div className="job-field">
+          <p>End Date:</p>
+          <p>{jobObj.period.end}</p>
+        </div>
+        <button
+          className="edit-button job-field"
+          onClick={() => {
+            this.handleEdit(context, jobObj);
+          }}
+        >
+          Edit
+        </button>
       </div>
     );
   }
